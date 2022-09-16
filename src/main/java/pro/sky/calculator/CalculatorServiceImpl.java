df70cd70fcb053a9a2b4df60c8e52aab429e162b
Pavel <vasilyev.x@mail.com>
2022-09-16 22:18:22 +0300

    Homework

--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -3,23 +3,4 @@ package pro.sky.calculator;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorServiceImpl implements CalculatorService {
-    public double answerPlus(double num1, double num2) {
-        return num1 + num2;
-    }
-
-    public double answerMinus(double num1, double num2) {
-        return num1 - num2;
-    }
-
-    public double answerMultiply(double num1, double num2) {
-        return num1 * num2;
-    }
-
-    public double answerDivide(double num1, double num2) {
-        if (num2 == 0) {
-            throw new RuntimeException("Cannot divide by zero!");
-        }
-        return num1 / num2;
-    }
-}
+public class CalculatorServiceImpl implements CalculatorService {}
