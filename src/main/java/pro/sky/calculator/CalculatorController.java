07bc5167f286259c8b77dfb8b0da8dad1388c2cd
Pavel <vasilyev.x@mail.com>
2022-09-16 22:16:00 +0300

    Homework

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,4 +1,36 @@
 package pro.sky.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping(path = "/plus")
+    public double answerPlus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return calculatorService.answerPlus(num1, num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public double answerMinus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return calculatorService.answerMinus(num1, num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public double answerMultiply(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return calculatorService.answerMultiply(num1, num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public double answerDivide(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return calculatorService.answerDivide(num1, num2);
+    }
 }
---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,12 @@
+package pro.sky.calculator;
+
+
+public interface CalculatorService {
+    double answerPlus(double num1, double num2);
+
+    double answerMinus(double num1, double num2);
+
+    double answerMultiply(double num1, double num2);
+
+    double answerDivide(double num1, double num2);
+}
